5a13cc180040979114a2106b252e28d4c4d51272
Tauqeer Ahmad <tauqeer.ahmad@Powersoft19.com>
2019-10-16 16:36:43 +0500

    Add userId in api/values
    Added an example in api/values end point

--- TRMDataManager/Controllers/ValuesController.cs
+++ TRMDataManager/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,7 +14,9 @@ namespace TRMDataManager.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            return new string[] { "value1", "value2", userId };
         }
 
         // GET api/values/5
